0b06566302e9b7610657298abb83b5ff9b87deed
Eli Lindsey <eli@siliconsprawl.com>
2020-08-02 15:03:39 -0400

    move inverse computation out of inner loop

--- src/ecl_rt.c
+++ src/ecl_rt.c
@@ -173,14 +173,16 @@ int main(void)
         for (u32 image_y = 0; image_y < height; ++image_y) {
             u32 *pixel = &pixels_private[image_y * width];
             for (u32 image_x = 0; image_x < width; ++image_x) {
+                f32 inverse_height = 1 / (height - 1.0f);
+                f32 inverse_width = 1 / (width - 1.0f);
 
                 v3 color = {0, 0, 0};
                 for (u32 rcount = 0; rcount < rays_per_pixel; ++rcount) {
                     // calculate ratio we've moved along the image (y/height), step proportionally within the viewport
                     f32 rand_x = randf01(&rand_state);
                     f32 rand_y = randf01(&rand_state);
-                    v3 viewport_y = v3_mulf(cam.y, cam.viewport_height * (image_y + rand_y) / (height - 1.0f));
-                    v3 viewport_x = v3_mulf(cam.x, cam.viewport_width * (image_x + rand_x) / (width - 1.0f));
+                    v3 viewport_y = v3_mulf(cam.y, cam.viewport_height * (image_y + rand_y) * inverse_height);
+                    v3 viewport_x = v3_mulf(cam.x, cam.viewport_width * (image_x + rand_x) * inverse_width);
                     v3 viewport_p = v3_add(v3_add(cam.viewport_lower_left, viewport_y), viewport_x);
                     // remember that a pixel in float-space is a _range_. We want to send multiple rays within that range
                     // to do this we take the start of that range (what we calculated as the image projecting onto our viewport),
